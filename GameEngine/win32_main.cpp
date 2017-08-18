3f30c95b84e7833c447d6f66fefc4e57249b2503
Thai Quoc Nguyen <thai_ng@outlook.com>
2017-08-18 07:43:06 -0700

    Snek

--- GameEngine/win32_main.cpp
+++ GameEngine/win32_main.cpp
@@ -33,6 +33,11 @@ struct GameState
 	static const int rows = 12;
 	static const int cols = 19;
 
+	GameState()
+	{
+		makeBoard();
+	}
+
 	std::vector<Rect> boardGrid;
 	void makeBoard()
 	{
@@ -47,12 +52,112 @@ struct GameState
 		}
 	}
 
+	struct Node
+	{
+		int x = 5;
+		int y = 5;
+		Rect sprite{ x * cellSize, y * cellSize, cellSize, cellSize };
+		void draw(Buffer& buffer)
+		{
+			sprite.x = x * cellSize;
+			sprite.y = y * cellSize;
+			DrawRect<DrawType::Fill>(buffer, sprite, Color{ 0xffffffff });
+		}
+	};
+
+	Node playerHead;
+	std::vector<Node> playerBody;
+
 	void draw(Buffer& buffer)
 	{
 		for (auto& g : boardGrid)
 		{
 			DrawRect<DrawType::Fill>(buffer, g, Color{ 0xffffffff });
 		}
+
+		playerHead.draw(buffer);
+		for (auto& b : playerBody)
+		{
+			b.draw(buffer);
+		}
+	}
+
+	enum class Direction
+	{
+		Up,
+		Down,
+		Left,
+		Right
+	};
+
+	void processInput()
+	{
+		if (!inputs.empty())
+		{
+			playerBody.push_back(Node(playerHead));
+			std::rotate(std::begin(playerBody), std::prev(std::end(playerBody)), std::end(playerBody));
+
+			Direction currentDirection = Direction::Up;
+			for (auto& input : inputs)
+			{
+				switch (input)
+				{
+				case KeyInput::Up:
+				{
+					currentDirection = Direction::Up;
+				} break;
+
+				case KeyInput::Down:
+				{
+					currentDirection = Direction::Down;
+				} break;
+
+				case KeyInput::Left:
+				{
+					currentDirection = Direction::Left;
+				} break;
+
+				case KeyInput::Right:
+				{
+					currentDirection = Direction::Right;
+				} break;
+
+				default:
+					break;
+				}
+			}
+
+			switch (currentDirection)
+			{
+			case Direction::Up:
+			{
+				playerHead.y += 1;
+			} break;
+
+			case Direction::Down:
+			{
+				playerHead.y -= 1;
+			} break;
+
+			case Direction::Left:
+			{
+				playerHead.x -= 1;
+			} break;
+
+			case Direction::Right:
+			{
+				playerHead.x += 1;
+			} break;
+
+			default:
+				break;
+			}
+		}
+	}
+
+	void update()
+	{
+		processInput();
 	}
 };
 
@@ -129,6 +234,7 @@ WinMain(HINSTANCE Instance,
 		}
 
 		// Do game update
+		game.update();
 
 		auto currentTime = std::chrono::high_resolution_clock::now();
 		auto elapsedTime = std::chrono::duration_cast<std::chrono::milliseconds>(currentTime - lastRenderTime);
